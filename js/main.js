90240cfd9b3ed99ec9bbaab566eb7325a7964079
Ian Donahue <donahue.ian@gmail.com>
2016-06-13 15:05:36 -0700

    updated graphic on landing page to include log and lat

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    generateArtistData();
+
+});
+
+
+
+function generateArtistData() {
+    // Variables for Artist Card
+    var artist = "Adele";
+    var artistProfession = "Singer-Songwriter";
+    var artistDOB = "May 15, 1988";
+    var artistAge = "27";
+    var artistPartner = "Simon Konecki";
+    var artistChildren = "Angelo Konecki";
+x
+    $('#artistName').html(artist);
+    console.log("artists Name");
+}
